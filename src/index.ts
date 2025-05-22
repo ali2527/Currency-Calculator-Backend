6c060b6e949f80f4b4397d3ddf02fa881f8ca5d2
Ali Hussain <56451349+ali2527@users.noreply.github.com>
2025-05-22 14:53:25 +0500

    cors update 1

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+
+const allowedOrigins = [
+  'https://currency-calculator-frontend-sigma.vercel.app'
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
 
 const PORT = process.env.PORT || 5000;  
 const API_KEY = process.env.CURRENCY_API_KEY;
